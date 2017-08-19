ed1f8d2fde70006682f6f2e7adc605d5c255f02e
Len Wanger <len_wanger@hotmail.com>
2017-08-19 00:39:34 -0500

    __version__ breaking conf.py for read the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,8 @@ import sys
 import datetime
 sys.path.insert(0, os.path.abspath('../cooked_input'))
 
-from cooked_input import __version__
+#from cooked_input import __version__
+__version__ = '0.1.1'
 
 today = datetime.date.today()
 today_str = today.strftime('%b %d, %Y')
