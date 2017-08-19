0a2bb0f07f5b39c16eeebe236d0cbcf51a3d5dbc
Len Wanger <len_wanger@hotmail.com>
2017-08-19 17:42:15 -0500

    zeroing in on read the docs issue.

--- docs/conf.py
+++ docs/conf.py
@@ -21,10 +21,8 @@ import os
 import sys
 import datetime
 
-sys.path.insert(0, os.path.abspath('../cooked_input'))
+#sys.path.insert(0, os.path.abspath('../cooked_input'))
 sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('../..'))
-#sys.path.insert(0, os.path.abspath('../cooked_input/cooked_input'))
 
 from cooked_input import __version__
 #__version__ = '0.1.3'
