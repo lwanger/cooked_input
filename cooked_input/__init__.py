b6afc15fa85500af19d374ffc87c199f36f1f781
Len Wanger <len_wanger@hotmail.com>
2017-08-20 23:36:17 -0500

    adding InAnyValidator to __init__.py

--- cooked_input/__init__.py
+++ cooked_input/__init__.py
@@ -8,7 +8,7 @@ from .get_input import TABLE_ID, TABLE_VALUE, TABLE_ID_OR_VALUE
 from .convertors import Convertor, IntConvertor, FloatConvertor, BooleanConvertor
 from .convertors import ListConvertor, DateConvertor, YesNoConvertor, TableConvertor
 from .validators import Validator, ExactLengthValidator, InLengthValidator, ExactValueValidator, InRangeValidator
-from .validators import NotInValidator, InChoicesValidator, RegexValidator, PasswordValidator, ListValidator
+from .validators import InAnyValidator, NotInValidator, InChoicesValidator, RegexValidator, PasswordValidator, ListValidator
 from .validators import in_all, in_any, not_in, validate
 from .cleaners import Cleaner, UpperCleaner, LowerCleaner, CapitalizeCleaner
 from .cleaners import StripCleaner, ReplaceCleaner
