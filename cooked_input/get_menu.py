c991c1d2e88e0c64ddf5cacf195b3c3ad09b91e7
Len Wanger <len_wanger@hotmail.com>
2017-09-14 09:09:52 -0500

    converted MenuChoice to object, Lots of changes including get_menu convenience funciton.

--- cooked_input/get_menu.py
+++ cooked_input/get_menu.py
@@ -59,10 +59,20 @@ from cooked_input.input_utils import put_in_a_list
 from cooked_input import ConvertorError
 
 
-MenuChoice = namedtuple("MenuChoice", "tag text action")
+# MenuChoice = namedtuple("MenuChoice", "tag text action")
 
 MENU_DEFAULT_ACTION = 'default'
 MENU_ACTION_QUIT = 'quit'
+
+
+class MenuChoice(object):
+    def __init__(self, text, tag=None, action=MENU_DEFAULT_ACTION):
+        self.text = text
+        self.tag = tag
+        self.action = action
+
+    def __repr__(self):
+        return 'MenuChoice(text={}, tag={}, action={})'.format(self.text, self.tag, self.action)
 
 
 class TableRowConvertor(Convertor):
@@ -88,8 +98,8 @@ class TableRowConvertor(Convertor):
         return 'TableRowConvertor(choices={}, value_error_str={})'.format(self._choices, self.value_error_str)
 
 
-class CIMenu(object):
-    def __init__(self, rows=(), default_action=None, **kwargs):
+class Menu(object):
+    def __init__(self, rows=(), title=None, prompt=None, default_choice='', default_action=None, **kwargs):
         try:
             self.add_quit = kwargs['add_quit']
         except KeyError:
@@ -105,6 +115,18 @@ class CIMenu(object):
         except KeyError:
             self.action_kwargs = {}
 
+        try:
+            self.case_sensitive = kwargs['case_sensitive']
+        except KeyError:
+            self.case_sensitive = False
+
+        if prompt is None:
+            self.prompt = 'Choose a menu item'
+        else:
+            self.prompt = prompt
+
+        self.title = title
+        self.default_choice = default_choice
         self.default_action = default_action
         self._rows = []
         self.tbl = pt.VeryPrettyTable()
@@ -115,50 +137,56 @@ class CIMenu(object):
         self.tbl.header = False
         self.tbl.align = 'l'
         self.tbl.align['tag'] = 'r'
-        self.tbl.left_padding_width = 2
+        # self.tbl.left_padding_width = 2
 
         for i, v in enumerate(rows):
             if v.tag is None:
                 r = ['{:3}'.format(i+1), v.text, v.action]
-                r2 = MenuChoice(i+1, v.text, v.action)
+                v.tag = i+1
             else:
                 r = [v.tag, v.text, v.action]
-                r2 = v
 
             self.tbl.add_row(r)
-            self._rows.append(r2)
+            self._rows.append(v)
 
         if self.add_quit:
             r = MenuChoice('quit', 'quit', MENU_ACTION_QUIT)
             self.tbl.add_row([r.tag, r.text, r.action])
             self._rows.append(r)
 
+    def __repr__(self):
+        return 'Menu(rows=..., title={}, prompt={}, default_choice={}, kwargs=...)'.format(self.title, self.prompt, self.default_choice)
 
     def get_numchoices(self):
         return len(self._rows)
-
 
     def get_action(self, tag):
         for row in self._rows:
             if row.tag == tag:
                 return row.action
-        raise ValueError('CIMenu.get_action: tag ({}) not in the menu'.format(tag))
+        raise ValueError('Menu.get_action: tag ({}) not in the menu'.format(tag))
 
     def _prep_get_input(self):
-        choices = tuple(c[0] for c in self._rows)
-        cleaners = [StripCleaner(), CapitalizationCleaner('lower'), ChoiceCleaner(choices)]
+        choices = tuple(c.tag for c in self._rows)
+
+        # cleaners = [StripCleaner(), CapitalizationCleaner('lower'), ChoiceCleaner(choices)]
+        cleaners = [StripCleaner()]
+        if not self.case_sensitive:
+            cleaners.append(CapitalizationCleaner('lower'))
+        cleaners.append(ChoiceCleaner(choices))
+
         convertor = TableRowConvertor(choices)
         validators = RangeValidator(min_val=0, max_val=len(choices))
         return choices, cleaners, convertor, validators
 
     def _get_choice(self, menu_choices, menu_cleaners, menu_convertor, menu_validators):
-        print('Menu:\n')
+        if self.title is not None:
+            print('{}'.format(self.title))
         print(self.tbl.get_string(fields=['tag', 'text']))  # don't show action
-        result = get_input(prompt='What would you like to do?: ', cleaners=menu_cleaners, convertor=menu_convertor,
-                           validators=menu_validators)
-        # print('result={}, action={}'.format(result, self.get_action(result-1)))
-        row = self._rows[result]
-        return row
+        result = get_input(prompt=self.prompt, cleaners=menu_cleaners, convertor=menu_convertor,
+                           validators=menu_validators, default=self.default_choice)
+
+        return self._rows[result]
 
     def get_menu_choice(self):
         menu_choices, menu_cleaners, menu_convertor, menu_validators = self._prep_get_input()
@@ -179,11 +207,22 @@ class CIMenu(object):
             elif callable(action):
                 action(choice.tag, self.action_args, self.action_kwargs)
             else:
-                print('CIMenu.run - no action specified for tag ({})'.format(choice.tag), file=sys.stderr)
+                print('Menu.run - no action specified for tag ({})'.format(choice.tag), file=sys.stderr)
         return True
 
 
-### test ####
+def get_menu(choices, title=None, prompt=None, default_choice='', add_quit=False, **kwargs):
+    menu_choices = [MenuChoice(choice) for choice in choices]
+    menu = Menu(menu_choices, title=None, prompt=None, default_choice='', add_quit=add_quit, **kwargs)
+    result = menu.get_menu_choice()
+
+    if add_quit and result=='quit':
+        return result
+
+    return menu_choices[result-1].text
+
+
+### tests ####
 def default_action(tag, *args, **kwargs):
     print('called default_action, tag={}, args={} kwargs={}'.format(tag, args, kwargs))
     return True
@@ -195,32 +234,47 @@ def action_1(tag, *args, **kwargs):
 
 def simple_menu():
     menu_choices = [
-        MenuChoice(None, "Choice 1", None),
-        MenuChoice(2, "Choice 2", MENU_DEFAULT_ACTION),
-        MenuChoice('foo', "Do Foo", MENU_DEFAULT_ACTION),
-        MenuChoice('bar', "Do Bar (action_1)", action_1),
-        MenuChoice('stop', "STOP the menu!", MENU_ACTION_QUIT),
+        MenuChoice("Choice 1", None, None),
+        MenuChoice("Choice 2", 2, MENU_DEFAULT_ACTION),
+        MenuChoice("Do Foo", 'foo', MENU_DEFAULT_ACTION),
+        MenuChoice("Do Bar (action_1)", 'bar', action_1),
+        MenuChoice("STOP the menu!", 'stop', MENU_ACTION_QUIT),
     ]
 
-    print('\nget_menu_choice - add_quit=True')
-    menu = CIMenu(menu_choices[:-1])
+    print('\nget_menu_choice - add_quit=True\n')
+    menu = Menu(menu_choices[:-1])
     choice = menu.get_menu_choice()
     print('choice={}, action={}'.format(choice, menu.get_action(choice)))
 
-    print('\nget_menu_choice - add_quit=False (no exit!)')
-    menu = CIMenu(menu_choices[:-1], add_quit=False)
+    print('\nget_menu_choice - add_quit=False (no exit!), case_sensitive=True, with title\n')
+    menu = Menu(menu_choices[:-1], title='My Menu:', add_quit=False, case_sensitive=True)
     choice = menu.get_menu_choice()
     print('choice={}, action={}'.format(choice, menu.get_action(choice)))
 
-    print('\nget_menu_choice - add_quit=False')
-    menu = CIMenu(menu_choices, default_action=default_action, add_quit=False)
+    print('\nget_menu_choice - add_quit=False, w/ prompt, default="stop"\n')
+    menu = Menu(menu_choices, prompt='Choose or die!', default_choice='stop', default_action=default_action, add_quit=False)
     choice = menu.get_menu_choice()
     print('choice={}, action={}'.format(choice, menu.get_action(choice)))
 
-    print('\nmenu.run - add_quit=False')
+    print('\nmenu.run - add_quit=True\n')
     menu.run()
     print('done')
 
+def test_get_menu():
+    # get_menu(choices, title=None, prompt=None, default_choice='', **kwargs):
+
+    choices = ['red', 'blue', 'green']
+
+    print('test_get_menu:\n')
+    print('simplest case:\n')
+    result = get_menu(choices)
+    print('result={}'.format(result))
+
+    print('\nwith options...\n')
+    result = get_menu(choices, title='My Menu', prompt="Choose m'lady", default_choice='red', add_quit=True)
+    print('result={}'.format(result))
+
 
 if __name__ == '__main__':
-    simple_menu()
+    test_get_menu()
+    # simple_menu()
