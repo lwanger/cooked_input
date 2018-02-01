8cff943d91975556ebafe6efc5bcfb26ecba9ab1
Len Wanger <len_wanger@hotmail.com>
2018-02-01 00:15:46 -0600

    cleaning up examples

--- cooked_input/examples/get_strs.py
+++ cooked_input/examples/get_strs.py
@@ -14,37 +14,19 @@ if __name__ == '__main__':
     good_flavors = ['cherry', 'lime', 'lemon', 'orange']
     bad_flavors = 'licorice'
 
-    length_3_validator = LengthValidator(min_len=3, max_len=3)
-    length_5_plus_validator = LengthValidator(min_len=5)
-    length_2_to_4_validator = LengthValidator(min_len=2, max_len=4)
-    choices_validator = ChoiceValidator(choices=colors)
     good_flavor_validator = ChoiceValidator(choices=good_flavors)
-    bad_flavor_validator = ChoiceValidator(choices=bad_flavors)
-    not_in_choices_validator = NoneOfValidator(validators=[bad_flavor_validator])
-
-    strip_cleaner = StripCleaner()
-    rstrip_cleaner = StripCleaner(lstrip=False, rstrip=True)
-    lower_cleaner = CapitalizationCleaner(style='lower')
-    upper_cleaner = CapitalizationCleaner(style='upper')
-    capitalize_cleaner = CapitalizationCleaner(style='first_word')
-    capitalize_all_cleaner = CapitalizationCleaner(style='all_words')
-    strip_and_lower_cleaners = [strip_cleaner, lower_cleaner]
+    not_in_choices_validator = NoneOfValidator(validators=[ChoiceValidator(choices=bad_flavors)])
+    strip_and_lower_cleaners = [StripCleaner(), CapitalizationCleaner(style='lower')]
 
 
     # simplest way
-    print(get_string())
+    print(get_string(prompt='Enter some text'))
 
-    # get any string
-    print(get_input(prompt='Enter any string'))
-    print(get_input(prompt='Enter any string', required=True))
-    print(get_input(prompt='Enter any string (will be stripped of leading and trailing spaces and converted to lower)',
-                    cleaners=strip_and_lower_cleaners))
+    # strip and capitalization cleaners
     print(get_input(prompt='Enter any string (will be stripped of trailing spaces and converted to upper)',
-                    cleaners=[rstrip_cleaner, upper_cleaner]))
+                    cleaners=[StripCleaner(lstrip=False, rstrip=True), CapitalizationCleaner(style='upper')]))
 
-    # capitalization cleaning (CapitalizationCleaner)
-    print(get_input(prompt='Enter your name (first word will be capitalized)', cleaners=capitalize_cleaner))
-    print(get_input(prompt='Enter your name (all words will be capitalized)', cleaners=capitalize_all_cleaner))
+    print(get_input(prompt='Enter your name (all words will be capitalized)', cleaners=CapitalizationCleaner(style='all_words')))
 
     # picking from choices (InchoicesValidator)
     prompt_str = "What is your favorite flavor jelly bean (pick any flavor, don't say licorice!)?"
@@ -54,10 +36,8 @@ if __name__ == '__main__':
     validators = [good_flavor_validator, not_in_choices_validator]
     print(get_input(prompt=prompt_str, cleaners=strip_and_lower_cleaners, validators=validators, default='cherry'))
 
-    # get different length strings
-    print(get_input(prompt='Enter a three letter string', validators=[length_3_validator]))
-    print(get_input(prompt='Enter a string at least 5 letters long', validators=[length_5_plus_validator]))
-    print(get_input(prompt='Enter a 2 to 4 letter string', validators=[length_2_to_4_validator]))
+    # specifying string length
+    print(get_input(prompt='Enter a 2 to 4 letter string', validators=[LengthValidator(min_len=2, max_len=4)]))
 
     # Use YesNoConvertor
-    print(get_input(prompt="Yes or no?", cleaners=strip_cleaner, convertor=YesNoConvertor(), default='Y'))
+    print(get_input(prompt="Yes or no?", cleaners=StripCleaner(), convertor=YesNoConvertor(), default='Y'))
