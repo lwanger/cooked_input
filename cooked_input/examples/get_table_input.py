1f79b256f16125e655c5ad722040d48b19d21a7a
Len Wanger <len_wanger@hotmail.com>
2018-02-01 00:38:56 -0600

    added more examples to get_table_input example and cleaning up.

--- cooked_input/examples/get_table_input.py
+++ cooked_input/examples/get_table_input.py
@@ -2,28 +2,82 @@
 cooked input examples of getting inputs from tables
 
 Len Wanger, 2017
+
+TODO -
+    - basic table
+    - basic table with tags
+    - dynamic table
+    - title, header and footer
+    - item_data
+    - rows_per_page
+    - enabled/disabled
+    - filtered
+    - actions
+    - navigation keys/commands
 """
 
-import cooked_input
-from cooked_input import get_table_input
+from cooked_input import get_table_input, Table, TableItem, get_menu, TABLE_RETURN_FIRST_VAL
+
+
+def return_color_str_action(row, action_dict):
+    return row.values[0]
+
+
+def red_action(row, action_dict):
+    if action_dict['live'] is True:
+        return 'Live is True!'
+    else:
+        return 'Better dead than red!'
+
+
+def return_rgb_action(row, action_dict):
+    return row.values[2]
 
 
 if __name__ == '__main__':
-    table = [(1, 'red'), (2, 'blue'), (4, 'green'), (6, 'yellow')]
-    cur_val = 'red'
+    table_items = [
+        TableItem('red'),
+        TableItem('blue'),
+        TableItem('green'),
+    ]
 
-    color = get_table_input(table=table, cleaners=None, convertor=None, validators=None, input_value=cooked_input.TABLE_ID, return_value=cooked_input.TABLE_ID, show_table=True,
-                             prompt='Enter the id of the color you want', value_error='a valid color id')
-    print('color id=%d' % (color))
+    # simplest way
+    table = Table(table_items, col_names=['Color'])
+    tag = get_table_input(table)
+    print('\ntag={}\n'.format(tag))
 
-    color = get_table_input(table=table, cleaners=None, convertor=None, validators=None, input_value=cooked_input.TABLE_ID, return_value=cooked_input.TABLE_VALUE, show_table=True,
-                                 default=cur_val, prompt='Enter the id of the color you want', value_error='a valid color id')
-    print('color=%s' % (color))
+    # get value from table with specified default action (get values[0] - color name)
+    table = Table(table_items, col_names=['Color'], default_action=TABLE_RETURN_FIRST_VAL, prompt='Color? (Table prompt) ')
+    color_str = get_table_input(table)
+    print('\ncolor_str={}\n'.format(color_str))
 
-    color = get_table_input(table=table, cleaners=None, convertor=None, validators=None, input_value=cooked_input.TABLE_VALUE, return_value=cooked_input.TABLE_VALUE, show_table=True,
-                                 default=cur_val, prompt='Enter the name of color you want', value_error='a valid color name')
-    print('color=%s' % (color))
+    # custom action for default table action and on a table item (red)
+    table_items[0] = TableItem('red', action=red_action)
+    table = Table(table_items, col_names=['Color'], default_action=return_color_str_action, action_dict={'live': False})
+    color = get_table_input(table, prompt='Color id')
+    print('\ncolor={}\n'.format(color))
 
-    color = get_table_input(table=table, cleaners=None, convertor=None, validators=None, input_value=cooked_input.TABLE_ID_OR_VALUE, return_value=cooked_input.TABLE_VALUE, show_table=True,
-                                 prompt='Enter the name or id of the color you want', value_error='a valid color name or id')
-    print('color=%s' % (color))
+    # running the table (loop until exit)
+    print('running table...')
+    table.run()
+    print('\ndone running table...\n')
+
+    # test default choice
+    result = get_menu(['red', 'green', 'blue'], title='Colors', prompt='Choose a color', default_choice='blue', add_exit=False)
+    print('result={}'.format(result))
+
+    # Test multiple columns
+    table_items = [
+        TableItem(['red', 'roses are red', (255,0,0)]),
+        TableItem(['blue', 'violets are blue', (0,255,0)]),
+        TableItem(['green', 'green is the color of my true love\'s eyes', (0,0,255)]),
+    ]
+
+    table = Table(table_items, col_names=['Color', 'Ode', 'RGB'], default_action=return_rgb_action)
+    rgb = get_table_input(table, prompt='Enter the id of the color you want',
+                            convertor_error_fmt='not a valid color id',
+                            validator_error_fmt='not a valid color id')
+    print('rgb={}, r={}, g={}, b={}'.format(rgb, rgb[0], rgb[1], rgb[2]))
+
+
+
